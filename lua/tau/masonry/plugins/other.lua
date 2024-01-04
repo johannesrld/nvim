b108944e7aa71ada6b44b0811e440deae8c8162b
johannesrld <71744831+johannesrld@users.noreply.github.com>
2024-01-04 16:03:42 +0800

    update

--- lua/tau/masonry/plugins/other.lua
+++ lua/tau/masonry/plugins/other.lua
@@ -9,9 +9,12 @@ return {
   },
   {
     'nvim-orgmode/orgmode',
+    lazy = true,
+    ft = { 'org', 'orgmode' },
     config = function()
       require('orgmode').setup_ts_grammar()
-      require('orgmode').setup {
+      require('orgmode').setup
+      {
         org_agenda_files = '~/orgfiles/**/*',
         org_default_notes_file = '~/orgfiles/refile.org',
       }
