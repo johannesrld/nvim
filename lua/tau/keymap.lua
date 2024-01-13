91b46cb6cee7a19e1446ffddd6c4b31b2f2d3155
johannesrld <71744831+johannesrld@users.noreply.github.com>
2024-01-13 13:01:14 +0800

    update

--- lua/tau/keymap.lua
+++ lua/tau/keymap.lua
@@ -1,6 +1,8 @@
+---@type function
 local set = vim.keymap.set
 ---@type function
 local hydra = require 'hydra'
+---@type function
 local wk = require 'which-key'
 
 wk.register({
@@ -16,6 +18,7 @@ wk.register({
 ---@return fun(customOpts: table): table
 local function defaultSetting(defaultOpts)
   return function(customOpts)
+    customOpts = customOpts or {}
     return vim.tbl_extend('force', defaultOpts, customOpts)
   end
 end
@@ -87,24 +90,38 @@ hydra {
 ---@param module string
 ---@return function
 local function Telescope(module)
+  local ivy_theme = require('telescope.themes').get_ivy{
+    layout_config = {
+      height = 13
+    },
+    borderchars = {
+      preview = { '─', '│', '─', '│', '┌', '┐', '┘', '└' },
+    }
+  }
   return function()
-    require('telescope.builtin')[module](require('telescope.themes').get_ivy { layout_config = { height = 10 } })
+    require('telescope.builtin')[module](ivy_theme)
   end
 end
+set('n', "[d", vim.diagnostic.goto_next, opts { desc = "Goto next issue" })
+set('n', "]d", vim.diagnostic.goto_prev, opts { desc = "Goto prev issue" })
 set('n', '<leader>tf', Telescope 'find_files', opts { desc = "Find Files" })
 set('n', '<leader>tg', Telescope 'live_grep', opts { desc = "Live grep" })
 set('n', '<leader>tb', Telescope 'buffers', opts { desc = "Current Buffers" })
+-- Note that the +l prefix is for "language", not lsp
 set('n', '<leader>lf', vim.lsp.buf.format, opts { desc = 'Format File' })
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     local opts = defaultSetting { buffer = ev.buf, silent = true, noremap = true }
-    set('n', 'gd', vim.lsp.buf.definition, opts { desc = "goto symbol definition" })
+
     set('n', 'K', vim.lsp.buf.hover, opts { desc = 'Hover Info' })
-    set('n', "[d", vim.diagnostic.goto_next, opts { desc = "Goto next issue" })
-    set('n', "]d", vim.diagnostic.goto_prev, opts { desc = "Goto prev issue" })
+    set('n', 'gd', vim.lsp.buf.definition, opts { desc = "goto symbol definition" })
+    set('n', 'gi', vim.lsp.buf.implementation, opts { desc = "goto implementation" })
+    set('n', 'gr', vim.lsp.buf.references, opts { desc = "goto references" })
+    set({ 'n', 'v' }, '<leader>la', vim.lsp.buf.code_action, opts { desc = 'Code Action' })
     set('n', '<leader>lr', vim.lsp.buf.rename, opts { desc = 'Rename Symbol' })
-    set('n', '<leader>la', vim.lsp.buf.code_action, opts { desc = 'Code Action' })
-    set('n', '<leader>ls', vim.lsp.buf.signature_help, opts { desc = 'Symbol Signature' })
+    set('n', '<leader>ls', Telescope 'lsp_document_symbols', opts { desc = 'View buffer symbols' })
+    -- set('n', '<leader>ls', Telescope 'lsp_workspace_symbols', opts { desc = 'View project symbols' })
+    -- set('n', '<leader>ls', vim.lsp.buf.signature_help, opts { desc = 'Symbol Signature' })
     -- set('n', '<leader>ct', vim.lsp.buf.type_definition, opts { desc = 'Jump to type definition' })
   end,
 })
