6fc4f3c16c6c33c5f23f66450a0bf60076d068e1
johannesrld <71744831+johannesrld@users.noreply.github.com>
2024-04-11 17:35:56 +0800

    add more haskell nonsense

--- lua/tau/plugins/language-plugins/haskell.lua
+++ lua/tau/plugins/language-plugins/haskell.lua
@@ -5,5 +5,15 @@ return {
         ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' }
     },
     { 'neovimhaskell/haskell-vim' },
-    { 'itchyny/vim-haskell-indent' }
+    { 'itchyny/vim-haskell-indent' },
+    {
+        'mrcjkb/haskell-snippets.nvim',
+        ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' },
+        lazy = true,
+        init = function()
+            local ls = require('luasnip')
+            local haskell_snippets = require('haskell-snippets').all
+            ls.add_snippets('haskell', haskell_snippets, { key = 'haskell' })
+        end
+    }
 }
