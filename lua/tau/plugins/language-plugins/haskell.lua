58baae9b458d86bafdb9e0a598f1507b93a06baf
johannesrld <71744831+johannesrld@users.noreply.github.com>
2024-04-11 08:11:29 +0800

    Fuck it we ball

---
+++ lua/tau/plugins/language-plugins/haskell.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        'MrcJkb/haskell-tools.nvim',
+        version = "^3",
+        ft = { 'haskell', 'lhaskell', 'cabal', 'cabalproject' }
+    },
+    { 'neovimhaskell/haskell-vim' },
+    { 'itchyny/vim-haskell-indent' }
+}
