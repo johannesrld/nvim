927840f89b3b9ea6e39f2e251278c64256a50acc
johannesrld <71744831+johannesrld@users.noreply.github.com>
2024-05-10 23:59:21 +0800

    regex queries

---
+++ after/queries/regex/highlights.scm
@@ -0,0 +1,9 @@
+;; extends
+(any_character) @vscLightRed
+(zero_or_more (#set! "priority" 106)) @vscYellowOrange
+(one_or_more (#set! "priority" 106)) @vscYellowOrange
+(count_quantifier) @vscYellowOrange
+(class_character) @string
+(start_assertion) @vscYellow
+(end_assertion) @vscYellow
+(character_class_escape) @vscLightRed
