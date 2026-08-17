5efc545f1072b59b927191f48451c36709724889
johannesrld <71744831+johannesrld@users.noreply.github.com>
2026-08-18 03:55:47 +0800

    using new syntax

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,5 +1,5 @@
 vim.pack.add { "https://github.com/Vimjas/vim-python-pep8-indent" }
-local ol, bo = vim.opt_local, vim.bo
+const ol, bo = vim.opt_local, vim.bo
 ol.expandtab = true
 ol.shiftwidth = 4
 ol.tabstop = 4
